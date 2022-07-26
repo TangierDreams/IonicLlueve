709dde46a179733bf6dc62bbd95028bf59425491
Luis Hidalgo <luistanger@gmail.com>
2022-07-26 23:11:22 +0200

    Inicio del proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
 
-  constructor() {}
+    public subscription: Subscription;
+    public prevision$: Observable<any>;
+    public prevision;
+
+
+    constructor(
+        public navController: NavController,
+        public httpClient: HttpClient
+    ) { }
+
+    ngOnInit(): void {
+        console.log("ngOnInit()");
+
+        const API_KEY: string = "b6f36a3894fcea73d145adf6cad063f5";
+        const RUTA_FORECAST: string = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,alert&units=metric&lang=ES&appid=";
+
+        this.prevision$ = this.httpClient.get(RUTA_FORECAST + API_KEY);
+        this.subscription = this.prevision$
+            .subscribe(datos => {
+                this.prevision = datos;
+                console.log(datos);
+            })
+    }
+
+
+    ngOnDestroy(): void {
+        console.log("ngOnDestroy()");
+        this.subscription.unsubscribe();
+
+    }
 
 }
